37dc2478b72baefb6b6cd7bfde2cbe2540f4e7dd
FrancescoGalanti <galanti_francesco>
2020-10-30 14:57:28 +0100

    secondo esercizio fatto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,19 +8,43 @@
  // Jquerys //
 
  $(document).ready(function(){
-
+   // primo esercizio //
 
   // oggetto studente //
-  var studenti = {
-    eta: "19",
-    cognome: "Galanti",
-    nome: "Francesco"
+  // var studenti = {
+  //   eta: "19",
+  //   cognome: "Galanti",
+  //   nome: "Francesco"
+  // }
+  //
+  // for(var key in studenti){
+  //    console.log(key);
+  //    console.log(studenti[key]);
+  // };
+
+  // secondo esercizio //
+
+  var classe = {
+      nome :"Dario",
+      cognome :"Dunzioni",
+      eta : "",
+      nomesecondo : "Sara",
+      cognomesecondo : "Passeri",
+      etasecondo : "",
+      nometerzo : "Francesco",
+      cognometerzo  : "Galanti",
+      etaterzo : "",
   }
 
-  for(var key in studenti){
+
+
+  for(var key in classe){
      console.log(key);
-     console.log(studenti[key]);
-  };
+     console.log(classe[key])
+  }
+
+
+
 
 
 
